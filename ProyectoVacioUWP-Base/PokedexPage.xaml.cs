927f48f059730c6484defa5fe21f15f7a9630640
Alejandro Rodríguez Santos <alejandro.rodriguez38@alu.uclm.es>
2026-05-05 21:01:09 +0200

    dashboard de la pokedex y backend
    el backend recarga los pokemon que haya en la solucion, hay que ponerlos explicitamente en pokdex.cs

--- ProyectoVacioUWP-Base/PokedexPage.xaml.cs
+++ ProyectoVacioUWP-Base/PokedexPage.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Windows.UI.Xaml.Controls;
 
 namespace ProyectoVacioUWP_Base
@@ -7,6 +8,37 @@ namespace ProyectoVacioUWP_Base
         public PokedexPage()
         {
             this.InitializeComponent();
+            CargarPokemons();
+        }
+
+        private void CargarPokemons()
+        {
+            Type[] tiposPokemon =
+            {
+                typeof(EmpoleonARS)
+            };
+
+            foreach (Type tipo in tiposPokemon)
+            {
+                if (Activator.CreateInstance(tipo) is UserControl control)
+                {
+                    if (control is iPokemon pokemon)
+                    {
+                        pokemon.verFondo(false);
+                        pokemon.verFilaVida(false);
+                        pokemon.verFilaEnergia(false);
+                        pokemon.verPocionVida(false);
+                        pokemon.verPocionEnergia(false);
+                        pokemon.verNombre(false);
+                        pokemon.verEscudo(false);
+                        pokemon.activarAniIdle(true);
+                    }
+
+                    control.Width = 280;
+                    control.Height = 220;
+                    spPokemons.Children.Add(control);
+                }
+            }
         }
     }
 }
