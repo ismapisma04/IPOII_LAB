1b07c11253737b9cad69ebb97cb112f5740a4196
Alejandro Rodríguez Santos <alejandro.rodriguez38@alu.uclm.es>
2026-05-05 21:06:25 +0200

    adicion descripcion pokedex

--- ProyectoVacioUWP-Base/PokedexPage.xaml.cs
+++ ProyectoVacioUWP-Base/PokedexPage.xaml.cs
@@ -1,5 +1,9 @@
 using System;
+using Windows.UI;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 
 namespace ProyectoVacioUWP_Base
 {
@@ -20,25 +24,131 @@ namespace ProyectoVacioUWP_Base
 
             foreach (Type tipo in tiposPokemon)
             {
-                if (Activator.CreateInstance(tipo) is UserControl control)
+                if (Activator.CreateInstance(tipo) is UserControl control && control is iPokemon pokemon)
                 {
-                    if (control is iPokemon pokemon)
-                    {
-                        pokemon.verFondo(false);
-                        pokemon.verFilaVida(false);
-                        pokemon.verFilaEnergia(false);
-                        pokemon.verPocionVida(false);
-                        pokemon.verPocionEnergia(false);
-                        pokemon.verNombre(false);
-                        pokemon.verEscudo(false);
-                        pokemon.activarAniIdle(true);
-                    }
+                    PrepararPokemonParaPokedex(pokemon, control);
 
-                    control.Width = 280;
-                    control.Height = 220;
-                    spPokemons.Children.Add(control);
+                    Border tarjeta = CrearTarjetaPokemon(control, pokemon);
+                    spPokemons.Children.Add(tarjeta);
                 }
             }
+        }
+
+        private void PrepararPokemonParaPokedex(iPokemon pokemon, UserControl control)
+        {
+            pokemon.verFondo(false);
+            pokemon.verFilaVida(false);
+            pokemon.verFilaEnergia(false);
+            pokemon.verPocionVida(false);
+            pokemon.verPocionEnergia(false);
+            pokemon.verNombre(false);
+            pokemon.verEscudo(false);
+            pokemon.activarAniIdle(true);
+
+            control.Width = 260;
+            control.Height = 180;
+            control.HorizontalAlignment = HorizontalAlignment.Left;
+            control.VerticalAlignment = VerticalAlignment.Center;
+            control.IsHitTestVisible = false;
+        }
+
+        private Border CrearTarjetaPokemon(UserControl control, iPokemon pokemon)
+        {
+            Grid contenido = new Grid
+            {
+                ColumnSpacing = 20
+            };
+
+            contenido.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(280) });
+            contenido.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            Border zonaClickPokemon = new Border
+            {
+                Background = new SolidColorBrush(Color.FromArgb(55, 255, 255, 255)),
+                BorderBrush = new SolidColorBrush(Colors.Black),
+                BorderThickness = new Thickness(4),
+                CornerRadius = new CornerRadius(8),
+                Padding = new Thickness(10),
+                MinHeight = 190,
+                Child = new Viewbox
+                {
+                    Stretch = Stretch.Uniform,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Child = control
+                }
+            };
+
+            Grid.SetColumn(zonaClickPokemon, 0);
+
+            StackPanel panelTexto = new StackPanel
+            {
+                VerticalAlignment = VerticalAlignment.Center,
+                Spacing = 10
+            };
+
+            TextBlock titulo = new TextBlock
+            {
+                Text = pokemon.Nombre,
+                FontSize = 28,
+                FontWeight = Windows.UI.Text.FontWeights.Bold,
+                Foreground = new SolidColorBrush(Colors.White),
+                TextWrapping = TextWrapping.WrapWholeWords
+            };
+
+            TextBlock subtitulo = new TextBlock
+            {
+                Text = "Descripción",
+                FontSize = 16,
+                FontWeight = Windows.UI.Text.FontWeights.SemiBold,
+                Foreground = new SolidColorBrush(Color.FromArgb(220, 191, 219, 254))
+            };
+
+            TextBlock descripcion = new TextBlock
+            {
+                Text = pokemon.Descripcion,
+                FontSize = 15,
+                Foreground = new SolidColorBrush(Color.FromArgb(235, 255, 255, 255)),
+                TextWrapping = TextWrapping.WrapWholeWords,
+                MaxWidth = 520
+            };
+
+            panelTexto.Children.Add(titulo);
+            panelTexto.Children.Add(subtitulo);
+            panelTexto.Children.Add(descripcion);
+
+            Grid.SetColumn(panelTexto, 1);
+
+            contenido.Children.Add(zonaClickPokemon);
+            contenido.Children.Add(panelTexto);
+
+            Border tarjetaExterior = new Border
+            {
+                Background = new SolidColorBrush(Color.FromArgb(45, 255, 255, 255)),
+                BorderBrush = new SolidColorBrush(Color.FromArgb(90, 255, 255, 255)),
+                BorderThickness = new Thickness(1.5),
+                CornerRadius = new CornerRadius(18),
+                Padding = new Thickness(18),
+                Margin = new Thickness(0, 0, 0, 6),
+                Child = contenido
+            };
+
+            tarjetaExterior.Tapped += (s, e) => IrADetallePokemon(pokemon);
+            tarjetaExterior.PointerEntered += (s, e) =>
+            {
+                tarjetaExterior.Background = new SolidColorBrush(Color.FromArgb(65, 255, 255, 255));
+            };
+            tarjetaExterior.PointerExited += (s, e) =>
+            {
+                tarjetaExterior.Background = new SolidColorBrush(Color.FromArgb(45, 255, 255, 255));
+            };
+
+            return tarjetaExterior;
+        }
+
+        private void IrADetallePokemon(iPokemon pokemon)
+        {
+            Frame.Navigate(typeof(MisPokemonPage), pokemon);
         }
     }
 }
